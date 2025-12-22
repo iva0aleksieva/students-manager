91c7535064fb6b8124f1d49f77c397e8c46cfdb2
Dr. Jordan Jordanov <d_yugioh@abv.bg>
2025-12-22 16:32:33 +0200

    Update SlidoController.cs

--- StudentsManager.Mvc/Controllers/SlidoController.cs
+++ StudentsManager.Mvc/Controllers/SlidoController.cs
@@ -11,14 +11,16 @@ namespace StudentsManager.Mvc.Controllers
     {
         private readonly IForumService _service;
 
+        private readonly record struct QuestionInput(string Question);
+
         public SlidoController(IForumService service)
         {
             _service = service;
         }
 
-        // GET: api/Slido?limit=20&skip=0
-        [HttpGet]
-        public async Task<IActionResult> GetQuestions([FromQuery] int limit = 20, int skip = 0)
+        // GET: api/Slido/questions/20/0
+        [HttpGet("questions/{limit:int}/{skip:int}")]
+        public async Task<IActionResult> GetQuestions([FromRoute] int limit, [FromRoute] int skip)
         {
             var result = await _service.GetAsync(limit, skip);
             return Ok(result);
@@ -26,16 +28,18 @@ namespace StudentsManager.Mvc.Controllers
 
         // POST: api/Slido/question
         [HttpPost("question")]
-        public async Task<IActionResult> PostQuestion([FromBody] string question)
+        public async Task<IActionResult> PostQuestion([FromBody] QuestionInput input)
         {
-            if (string.IsNullOrEmpty(question))
+            if (string.IsNullOrWhiteSpace(input.Question))
                 return BadRequest("Question cannot be empty");
 
             await _service.SaveQuestionAsync(
-                question,
+                input.Question,
                 Guid.Parse("1eac9820-5e6e-4d10-6e94-08de36f40f78"));
 
             return Ok("Question posted successfully");
         }
     }
 }
+
+public readonly record struct QuestionInput(string Question);
